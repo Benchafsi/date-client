80d4512e197f9be878f523dbcfe81e67964c54ed
Benchafsi Giasin <giasinbenchafsi@gmail.com>
2025-01-26 13:54:39 +0200

    commit

---
+++ src/app/nav copy/nav.component.ts
@@ -0,0 +1,39 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [
+    FormsModule,
+    //sDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    //asRoleDirective,
+  ],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css',
+})
+export class NavComponent {
+  accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+  model: any = {};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: (_) => {
+        this.router.navigateByUrl('/members');
+      },
+      error: (error) => this.toastr.error(error.error),
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+}
